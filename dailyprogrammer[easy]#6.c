4935b23b46a3b5c6f9551f094052333b8231d500
qual <denizsydgl@gmail.com>
2016-01-04 16:11:56 +0200

    4.day, bus final was alright i guess

---
+++ dailyprogrammer[easy]#6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+/*
+You're challenge for today is to create a program 
+that can calculate pi accurately to at least 30 decimal places.
+Try not to cheat :)
+*/
+void
+main(void){
+    printf("%.30lf\n", (2 * asin(1)));          //thank you highschool trigonometry
+}
