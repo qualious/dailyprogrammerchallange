b380d5f370ad7c2e86910ea6e5f3b21b1281e018
qual <denizsydgl@gmail.com>
2016-01-28 12:44:28 +0200

    #31 updated

--- dailyprogrammer[easy]#31.cpp
+++ dailyprogrammer[easy]#31.cpp
@@ -9,20 +9,25 @@ their product using the same notation. As an example, CSGHJ × CBA = FNEUZJA.
 Your task is to write the base-26 multiplication function.
 */
 
-char
-_result(char c1, char c2){
-}
-
 int
 main(int argc, char** argv){                                                                
     char str[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
             'I','J', 'K', 'L', 'M', 'N', 'O', 'P','Q','R','S','T','U','V','W','X','Y','Z'};
-    string s1(argv[1]); string s2(argv[2]); string s3;
+    string s1(argv[1]); string s2(argv[2]); string s3, s4,s5;
+    long int a = 0;
     for (unsigned int i = 0; i < s2.length(); ++i){
         for (int j = 0; j < s1.length(); ++j){
             s3.push_back(str[((s1[j] - 'A') * (s2[i] - 'A'))%26]);
         }
     }
-    cout << s3;
-    
+    for(unsigned int i = s2.length() * s1.length(); i > 0; i = i - s1.length()){
+        s4 = s3.substr(i-s1.length(),i);
+        if(s4.length() > s1.length())
+            s4 = s4.substr(0,s1.length());
+        for (unsigned int j = 0; j < s4.length(); ++j){
+            a *= 10;
+            a += s4[j] - 'A';
+            cout << static_cast<char>(a%10 + 65);
+        }
+    }
 }
