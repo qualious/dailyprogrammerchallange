30e2973cc115a49ac73d0589e209de0e86bdc058
qual <denizsydgl@gmail.com>
2016-02-07 18:58:26 +0200

    37.day, one more comming since this is same as #3

---
+++ dailyprogrammer[easy]#47.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+/*
+Your task today is to implement one of the oldest ciphers known, the so-called 
+Caesar cipher (or Caesar shift, as it is sometimes called). It works like this: 
+for every letter you want to encrypt, you shift it some number of places down the
+alphabet to get the letter in the cipher. */
+int
+main(void){
+    cout << "Look at the daily programmer #3";
+}
